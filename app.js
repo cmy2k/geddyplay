e12f0786904fea9c9d84a44b0bd1139c2c7c7cc5
cmy2k <jmhconsultingllc@gmail.com>
2014-08-08 15:47:09 -0400

    app wrapper for elastic beanstalk

---
+++ app.js
@@ -0,0 +1,9 @@
+// wrapper for elastic beanstalk
+
+var geddy = require('geddy');
+
+geddy.startCluster({
+  hostname: '0.0.0.0',
+  port: process.env.PORT || '3000',
+  environment: process.env.NODE_ENV || 'development'
+});
